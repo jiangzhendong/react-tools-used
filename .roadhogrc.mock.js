1665ded087d35d050d98569a33941c06ae33dd64
j.wind <jiangzhendong102@hotmail.com>
2018-04-19 16:09:55 +0800

    初次提交代码2

--- .roadhogrc.mock.js
+++ .roadhogrc.mock.js
@@ -1,3 +1,12 @@
-
+import { format, delay } from 'roadhog-api-doc';
+// 是否禁用代理
+// const noProxy = process.env.NO_PROXY === 'true';
+// const proxy = {};
+// export default noProxy ? {
+//     'GET /api/(.*)': 'http://127.0.0.1:8089/api',
+//     'POST /api/(.*)': 'http://127.0.0.1:8089/api',
+//   } : delay(proxy, 1000);
 export default {
-};
+  'GET /api/(.*)': 'http://127.0.0.1:8089/api',
+  'POST /api/(.*)': 'http://127.0.0.1:8089/api',
+}
